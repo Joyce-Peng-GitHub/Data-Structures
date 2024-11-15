8986e3b7a3bc5206fed3c9095796b4559e9059c9
Joyce <1071060893@qq.com>
2024-11-15 19:31:47 +0800

    Added min_segtree and max_segtree.

--- extreme_segtree.hpp
+++ extreme_segtree.hpp
@@ -10,6 +10,10 @@ namespace ds {
 			  elem_t init,
 			  typename comp_t = std::less<elem_t>>
 	using extreme_segtree = generic_segtree<elem_t, init, comp_t>;
+	template <typename elem_t, elem_t type_max>
+	using min_segtree = extreme_segtree<elem_t, type_max, std::less<elem_t>>;
+	template <typename elem_t, elem_t type_min>
+	using max_segtree = extreme_segtree<elem_t, type_min, std::greater<elem_t>>;
 }
 
 #endif
