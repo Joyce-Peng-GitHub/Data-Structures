e40e0fba400425c832c0db63c6034ab794661fc8
Joyce <1071060893@qq.com>
2024-12-17 20:58:09 +0800

    Finished it.

---
+++ ds/bigint.hpp
@@ -0,0 +1,17 @@
+#ifndef _BIGUINT_HPP
+#define _BIGUINT_HPP
+
+#include <cstdint>
+#include <vector>
+
+namespace ds {
+	class bigint {
+	public:
+	protected:
+		std::vector<uint64_t> m_data;
+
+	private:
+	};
+}
+
+#endif
