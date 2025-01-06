28da3c597234b0d89c2a15791cde733ecba8ffb1
Joyce <1071060893@qq.com>
2025-01-06 22:19:18 +0800

    Overloaded the bitwise or operator.

--- ds/bigint.hpp
+++ ds/bigint.hpp
@@ -36,7 +36,8 @@ namespace ds {
 				m_data.push_back(m_data[sz - 1] >> (BITS - n));
 			}
 			for (size_t i = sz + m - 1; i != m; --i) {
-				m_data[i] = ((m_data[i - m] << n) | (m_data[i - m - 1] >> (BITS - n)));
+				m_data[i] = ((m_data[i - m] << n) |
+							 (m_data[i - m - 1] >> (BITS - n)));
 			}
 			m_data[m] = (m_data[0] << n);
 			for (size_t i = 0; i != m; ++i) {
@@ -62,7 +63,8 @@ namespace ds {
 				return *this;
 			}
 			for (size_t i = 0; i + m + 1 != m_data.size(); ++i) {
-				m_data[i] = ((m_data[i + m] >> n) | (m_data[i + m + 1] << (BITS - n)));
+				m_data[i] = ((m_data[i + m] >> n) |
+							 (m_data[i + m + 1] << (BITS - n)));
 			}
 			if (m_data.back() >> n) {
 				m_data[m_data.size() - m - 1] = (m_data.back() >> n);
@@ -85,8 +87,57 @@ namespace ds {
 			}
 			return x;
 		}
+		inline bigint &operator&=(const bigint &rhs) {
+			size_t i = std::min(m_data.size(), rhs.m_data.size()) - 1;
+			while (~i && !(m_data[i] & rhs.m_data[i])) {
+				--i;
+			}
+			m_data.resize(i + 1);
+			for (; ~i; --i) {
+				m_data[i] &= rhs.m_data[i];
+			}
+			return *this;
+		}
+		inline friend bigint operator&(const bigint &lhs, const bigint &rhs) {
+			size_t i = std::min(lhs.m_data.size(), rhs.m_data.size()) - 1;
+			while (~i && !(lhs.m_data[i] & rhs.m_data[i])) {
+				--i;
+			}
+			bigint res;
+			res.m_data.resize(i + 1);
+			for (; ~i; --i) {
+				res.m_data[i] = (lhs.m_data[i] & rhs.m_data[i]);
+			}
+			return res;
+		}
+		inline bigint &operator|=(const bigint &rhs) {
+			size_t i = 0;
+			for (; i != m_data.size() && i != rhs.m_data.size(); ++i) {
+				m_data[i] |= rhs.m_data[i];
+			}
+			if (i != rhs.m_data.size()) {
+				m_data.insert(m_data.end(), rhs.m_data.begin() + i, rhs.m_data.end());
+			}
+			return *this;
+		}
+		inline friend bigint operator|(const bigint &lhs, const bigint &rhs) {
+			bigint res;
+			res.m_data.resize(std::max(lhs.m_data.size(), rhs.m_data.size()));
+			size_t i = 0;
+			for (; i != lhs.m_data.size() && i != rhs.m_data.size(); ++i) {
+				res.m_data[i] = (lhs.m_data[i] | rhs.m_data[i]);
+			}
+			for (; i != lhs.m_data.size(); ++i) {
+				res.m_data[i] = lhs.m_data[i];
+			}
+			for (; i < rhs.m_data.size(); ++i) {
+				res.m_data[i] = rhs.m_data[i];
+			}
+			return res;
+		}
 
-		inline friend std::ostream &testOut(std::ostream &os, const bigint &x) {
+		inline friend std::ostream &testOut(std::ostream &os,
+											const bigint &x) {
 			if (x.m_data.empty()) {
 				return os << 0;
 			}
@@ -99,6 +150,16 @@ namespace ds {
 
 	protected:
 		std::vector<uint64_t> m_data;
+
+		inline void m_expand(size_t n) { // Suppose n >= m_data.size()
+			if (m_data.empty()) {
+				m_data.resize(n);
+			} else if (m_data.back() & (static_cast<uint64_t>(1) << (BITS - 1))) {
+				m_data.resize(n, static_cast<uint64_t>(-1));
+			} else {
+				m_data.resize(n);
+			}
+		}
 	};
 }
 
