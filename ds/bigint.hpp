f08125c80a0d0c8e3125cd61bf17a52fc5392d55
Ritel <Ritel_T@outlook.com>
2025-01-11 01:53:14 +0800

    fixed merge error and slightly adjusted

--- ds/bigint.hpp
+++ ds/bigint.hpp
@@ -1,14 +1,15 @@
-#ifndef _BIGUINT_HPP
-#define _BIGUINT_HPP
+#ifndef DS_BIGINT_HPP_
+#define DS_BIGINT_HPP_
 
 #include <cstdint>
-#include <vector>
 #include <iostream>
 #include <string>
+#include <vector>
 
 namespace ds {
+
 	// compile-time log2
-	constexpr uint8_t log2(uint8_t n) {
+	static constexpr uint8_t log2(uint8_t n) {
 		uint8_t result = 0;
 		while (n > 1) {
 			n /= 2;
@@ -21,18 +22,16 @@ namespace ds {
 	public:
 		// unit_t is the type of each unit in the bigint
 		using unit_t = uint64_t;
-
 		// unit_bits is the number of bits in one unit_t
 		uint8_t static constexpr unit_bits = sizeof(unit_t) * 8;
-
-		// log_bits is log2(unit_bits),
+		// log_bits is used to optimize math calculations
 		uint8_t static constexpr log_bits = log2(unit_bits);
 
 		inline bigint() = default;
-		inline bigint(unit_t const& x) : m_data(1, x) {}
-		inline bigint(std::string const& str) {
+		inline explicit bigint(const unit_t &x) : m_data(1, x) {}
+		inline bigint(const std::string &str) {
 			/* TODO: Implement the constructor from string */
-        }
+		}
 		/* TODO: Implement copy and move constructors */
 
 		inline bigint &operator<<=(size_t n) {
@@ -41,12 +40,12 @@ namespace ds {
 			}
 			size_t m = (n >> log_bits); // m = n / unit_bits
 			n &= unit_bits - 1;			// n %= unit_bits
-			/*
-			 * A right shift by 64 bits leads to undefined behavior.
-			 * So we need to handle separately the case that n == 0
-			 * to avoid this problem.
-			 */
 			if (!n) {
+				/*
+				 * A right shift by 64 bits leads to undefined behavior.
+				 * So we need to handle separately the case that n == 0
+				 * to avoid this problem.
+				 */
 				m_data.insert(m_data.begin(), m, 0);
 				return *this;
 			}
@@ -57,7 +56,7 @@ namespace ds {
 			}
 			for (size_t i = sz + m - 1; i != m; --i) {
 				m_data[i] = ((m_data[i - m] << n) |
-				 			 (m_data[i - m - 1] >> (unit_bits - n)));
+							 (m_data[i - m - 1] >> (unit_bits - n)));
 			}
 			m_data[m] = (m_data[0] << n);
 			for (size_t i = 0; i != m; ++i) {
@@ -67,105 +66,6 @@ namespace ds {
 		}
 		inline friend bigint operator<<(bigint lhs, size_t n) {
 			return (lhs <<= n);
-		}
-		inline bigint &operator>>=(size_t n) {
-			if (m_data.empty()) {
-				return *this;
-			}
-			size_t m = (n >> LOG_BITS); // m = n / BITS
-			if (m >= m_data.size()) {
-				m_data.clear();
-				return *this;
-			}
-			n &= BITS - 1; // n %= BITS
-			if (!n) {
-				m_data.erase(m_data.begin(), m_data.begin() + m);
-				return *this;
-			}
-			for (size_t i = 0; i + m + 1 != m_data.size(); ++i) {
-				m_data[i] = ((m_data[i + m] >> n) |
-							 (m_data[i + m + 1] << (BITS - n)));
-			}
-			if (m_data.back() >> n) {
-				m_data[m_data.size() - m - 1] = (m_data.back() >> n);
-			} else {
-				++m;
-			}
-			m_data.resize(m_data.size() - m);
-			return *this;
-		}
-		inline friend bigint operator>>(bigint lhs, size_t n) {
-			return (lhs >>= n);
-		}
-		inline friend bigint operator~(bigint x) {
-			size_t i = x.m_data.size() - 1;
-			for (; ~i && x.m_data[i] == static_cast<uint64_t>(-1); --i) {
-				x.m_data.pop_back();
-			}
-			for (; ~i; --i) {
-				x.m_data[i] = ~x.m_data[i];
-			}
-			return x;
-		}
-		inline bigint &operator&=(const bigint &rhs) {
-			size_t i = std::min(m_data.size(), rhs.m_data.size()) - 1;
-			while (~i && !(m_data[i] & rhs.m_data[i])) {
-				--i;
-			}
-			m_data.resize(i + 1);
-			for (; ~i; --i) {
-				m_data[i] &= rhs.m_data[i];
-			}
-			return *this;
-		}
-		inline friend bigint operator&(const bigint &lhs, const bigint &rhs) {
-			size_t i = std::min(lhs.m_data.size(), rhs.m_data.size()) - 1;
-			while (~i && !(lhs.m_data[i] & rhs.m_data[i])) {
-				--i;
-			}
-			bigint res;
-			res.m_data.resize(i + 1);
-			for (; ~i; --i) {
-				res.m_data[i] = (lhs.m_data[i] & rhs.m_data[i]);
-			}
-			return res;
-		}
-		inline bigint &operator|=(const bigint &rhs) {
-			size_t i = 0;
-			for (; i != m_data.size() && i != rhs.m_data.size(); ++i) {
-				m_data[i] |= rhs.m_data[i];
-			}
-			if (i != rhs.m_data.size()) {
-				m_data.insert(m_data.end(), rhs.m_data.begin() + i, rhs.m_data.end());
-			}
-			return *this;
-		}
-		inline friend bigint operator|(const bigint &lhs, const bigint &rhs) {
-			bigint res;
-			res.m_data.resize(std::max(lhs.m_data.size(), rhs.m_data.size()));
-			size_t i = 0;
-			for (; i != lhs.m_data.size() && i != rhs.m_data.size(); ++i) {
-				res.m_data[i] = (lhs.m_data[i] | rhs.m_data[i]);
-			}
-			for (; i != lhs.m_data.size(); ++i) {
-				res.m_data[i] = lhs.m_data[i];
-			}
-			for (; i < rhs.m_data.size(); ++i) {
-				res.m_data[i] = rhs.m_data[i];
-			}
-			return res;
-		}
-
-		inline friend std::ostream &testOut(std::ostream &os,
-											const bigint &x) {
-			if (x.m_data.empty()) {
-				return os << 0;
-			}
-			os << x.m_data.back();
-			for (size_t i = x.m_data.size() - 2; ~i; --i) {
-				os << ' ' << x.m_data[i];
-			}
-			return os;
 		}
 
 		inline bigint &operator>>=(size_t n) {
@@ -208,6 +108,7 @@ namespace ds {
 			}
 			return x;
 		}
+
 		inline bigint &operator&=(const bigint &rhs) {
 			size_t i = std::min(m_data.size(), rhs.m_data.size()) - 1;
 			while (~i && !(m_data[i] & rhs.m_data[i])) {
@@ -231,6 +132,7 @@ namespace ds {
 			}
 			return res;
 		}
+
 		inline bigint &operator|=(const bigint &rhs) {
 			size_t i = 0;
 			for (; i != m_data.size() && i != rhs.m_data.size(); ++i) {
@@ -257,12 +159,11 @@ namespace ds {
 			return res;
 		}
 
-	    inline bool operator==(const bigint &other) {
-            return m_data == other.m_data;
-        }
+		inline bool operator==(const bigint &other) const {
+			return m_data == other.m_data;
+		}
 
-		inline friend std::ostream &testOut(std::ostream &os,
-											const bigint &x) {
+		inline friend std::ostream &testOut(std::ostream &os, const bigint &x) {
 			if (x.m_data.empty()) {
 				return os << 0;
 			}
@@ -279,13 +180,14 @@ namespace ds {
 		inline void m_expand(size_t n) { // Suppose n >= m_data.size()
 			if (m_data.empty()) {
 				m_data.resize(n);
-			} else if (m_data.back() & (static_cast<uint64_t>(1) << (BITS - 1))) {
+			} else if (m_data.back() & (static_cast<uint64_t>(1) << (unit_bits - 1))) {
 				m_data.resize(n, static_cast<uint64_t>(-1));
 			} else {
 				m_data.resize(n);
 			}
 		}
-	};
-}
+	}; // class bigint
 
-#endif
+} // namespace ds
+
+#endif // _BIGUINT_HPP
